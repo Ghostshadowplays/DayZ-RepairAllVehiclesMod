c0a3a44da9fa4622149bc950e50bf96e4735a4d1
GhostShadow <125534518+Ghostshadowplays@users.noreply.github.com>
2024-11-28 14:12:10 +0000

    Delete FullVehicleRepair directory

--- FullVehicleRepair/Scripts/4_World/Actions/actionrepairvehiclewithblowtorchCB.c
+++
@@ -1,7 +0,0 @@
-class ActionRepairVehicleWithBlowtorchCB : ActionContinuousBaseCB
-{
-    override void CreateActionComponent()
-    {
-        m_ActionData.m_ActionComponent = new CAContinuousTime(10);
-    }
-};
